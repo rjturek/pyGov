4aa1152171e5c4143b754133e8816f87f34f88b8
Richard Turek <rjt1357@gmail.com>
2015-12-18 22:01:28 -0500

    http://127.0.0.1:5000/what

--- pyGov.py
+++ pyGov.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/what')
+def what():
+    return "What are we doing for the hackathon?"
 
 if __name__ == '__main__':
     app.run()
